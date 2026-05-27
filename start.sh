affd201ada35381b013f25377680b5fda0de82d5
technical <paperchor01@gmail.com>
2026-05-27 10:31:16 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
-# 1. Turn on the fake monitor (Resolution: 1280x720)
+# 1. Start the virtual frame buffer (the fake monitor)
+Xvfb :99 -screen 0 1024x768x16 &
 export DISPLAY=:99
-Xvfb :99 -screen 0 1280x720x24 &
-sleep 2
 
-# 2. Start the window manager (so the SUMO window has borders/buttons)
+# 2. Start the window manager (draws the grey background)
 fluxbox &
 
-# 3. Turn on the screen-capturing software
-x11vnc -display :99 -forever -nopw -bg -xkb
+# 3. Start the VNC server (plugs in the video cable)
+x11vnc -display :99 -nopw -listen localhost -xkb -ncache 10 -ncache_cr -forever &
 
-# 4. Run your JLN Marg SUMO simulation in the background
-python simulation.py &
+# 4. Start the web streaming software IN THE BACKGROUND
+websockify --web=/usr/share/novnc/ ${PORT} localhost:5900 &
 
-# 5. Start the web server to stream the video to your browser
-websockify --web=/usr/share/novnc/ ${PORT} localhost:5900
+# 5. Wait 5 seconds to let the monitor fully turn on
+sleep 5
+
+# 6. Run the AI engine IN THE FOREGROUND to trap the bug
+python simulation.py
